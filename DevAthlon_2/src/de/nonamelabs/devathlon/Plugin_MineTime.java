5d49467d71f5786a02ef73bdc48b1421ddd43892
Lau <NoNameLabs42@googlemail.com>
2015-07-17 18:19:04 +0200

    Push #32
    Start game etc

--- DevAthlon_2/src/de/nonamelabs/devathlon/Plugin_MineTime.java
+++ DevAthlon_2/src/de/nonamelabs/devathlon/Plugin_MineTime.java
@@ -4,27 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.nonamelabs.devathlon.minetime.Game;
+import de.nonamelabs.devathlon.minetime.Items;
 import de.nonamelabs.devathlon.util.Coordinate;
 
-public class Plugin_MineTime extends JavaPlugin {
+public class Plugin_MineTime extends JavaPlugin implements Listener {
 	public static Plugin_MineTime Plugin;
 	public static Logger logger;
 	
 	public Game game;
-	public List<Coordinate> rooms = new ArrayList<Coordinate>();
+	public List<Coordinate> room_list = new ArrayList<Coordinate>();
 	
 	public void onEnable() {
 		Plugin = this;
 		logger = getLogger();
 	
+		getServer().getPluginManager().registerEvents(this, this);
+		
 		loadConfig();
 		logger.info("Plugin wurde gestartet!");
 	}
@@ -37,6 +47,11 @@ public class Plugin_MineTime extends JavaPlugin {
 	public void save_Config() {
 		FileConfiguration config = getConfig();
 		
+		config.set("rooms", room_list.size());
+		
+		for (int i = 1; i <= room_list.size(); i++) {
+			room_list.get(i-1).savetoConfig(config, i);
+		}
 		
 		saveConfig();
 	}
@@ -44,6 +59,12 @@ public class Plugin_MineTime extends JavaPlugin {
 	public void loadConfig() {
 		FileConfiguration config = getConfig();
 				
+		int size = config.getInt("rooms");
+		
+		for (int i = 1; i <= size; i++) {
+			room_list.add(new Coordinate(config, i));
+		}
+		
 		saveConfig();
 	}
 	
@@ -71,10 +92,30 @@ public class Plugin_MineTime extends JavaPlugin {
 				return false;
 			}
 			
+			if (rooms < 1) {
+				sender.sendMessage(ChatColor.RED + "Es muss mindestens einen Raum geben!");
+				return false;
+			}
+			
+			if (rooms > room_list.size()) {
+				sender.sendMessage(ChatColor.RED + "Es wurden noch nicht genügend Räume hinzugefügt! Mit /selector bekommst du ein Item mit dem du neue Räume hinzufügst!");
+				return false;
+			}
+			
+			if (time < 5) {
+				sender.sendMessage(ChatColor.RED + "Es müssen mindestends 5 Sekunden Zeit sein!");
+				return false;
+			}
+			
+			if (Bukkit.getOnlinePlayers().size() < 2) {
+				sender.sendMessage(ChatColor.RED + "Es müssen mindestends 2 Spieler online sein!");
+				return false;
+			}
+			
 			if (game!=null) {
 				sender.sendMessage(ChatColor.RED + "Das Spiel ist bereits gestartet!");
 			} else {
-				game = new Game(rooms, time);
+				game = new Game(rooms, time, room_list);
 				sender.sendMessage(ChatColor.GREEN + "Du hast das Spiel gestartet!");
 			}
 			return true;
@@ -88,21 +129,40 @@ public class Plugin_MineTime extends JavaPlugin {
 				sender.sendMessage(ChatColor.GREEN + "Du hast das Spiel gestoppt!");
 			}
 			return true;
-		} else if (commandlabel.equalsIgnoreCase("addRoom")) {
-			if (!(sender instanceof Player)) {
-				
-				
+		} else if (commandlabel.equalsIgnoreCase("selector")) {
+			if (sender instanceof Player) {
+				((Player)sender).getInventory().addItem(Items.getSelectorItem());
+				sender.sendMessage(ChatColor.GREEN + "Du hast das Selector-Item erhalten! Rechtsklicke die Nord_West_Tiefste Ecke des Raums (der Raum muss 24 * 24 * 24 Blöcke groß sein und 3 Etagen beinhalten) um einen Raum hinzuzufügen!");
 				return true;	
 			} else {
 				sender.sendMessage(ChatColor.RED + "Du bist kein Spieler!");
 				return false;
 			}
 		} else if (commandlabel.equalsIgnoreCase("resetRooms")) {
-			rooms.clear();
-			sender.sendMessage(ChatColor.GREEN + "Alle Raeume wurden zurueckgesetzt!");
+			room_list.clear();
+			sender.sendMessage(ChatColor.GREEN + "Alle Räume wurden zurückgesetzt!");
 			return true;
 		} 
 		
 		return false;
+	}
+	
+	@EventHandler
+	public void onPlayerAction(PlayerInteractEvent event) {
+		ItemStack item = event.getItem();
+		if (item == null) return;
+		
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			if (item.equals(Items.getSelectorItem())) {
+				event.setCancelled(true);
+				addRoom(event.getPlayer(), event.getClickedBlock().getLocation());
+			}
+		}
+	}
+	
+	public void addRoom(Player p, Location l) {
+		Coordinate c = new Coordinate(l.getBlockX(), l.getBlockY(), l.getBlockZ());
+		room_list.add(c);
+		p.sendMessage(ChatColor.GREEN + "Es wurde ein Raum bei " + c + " hinzugefügt! Es gibt jetzt " + room_list.size() + " Räume!");
 	}
 }
